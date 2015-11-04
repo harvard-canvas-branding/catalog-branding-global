6c5e76eca14164b1a67824cc160228d13243621d
Colin Murtaugh <cmurtaugh@gmail.com>
2015-11-04 17:10:20 -0500

    Update global.js
    Added snippet from Instructure to change the dashboard link so that it goes to the Canvas dashboard rather than the Catalog dashboard.

--- global.js
+++ global.js
@@ -3,3 +3,10 @@ $(document).ready(function(e) {
     harvardCopy += '<p><a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Privacy+Policy+for+Canvas" id="privacy_policy_link">Privacy Policy</a> | <a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Acceptable+Use+Policy+for+Canvas" id="acceptable_use_policy_link">Acceptable Use Policy</a></p>';
     $('footer').html(harvardCopy);    
 });
+
+$(document).ready(function() {
+ $("#global_nav_dashboard_link").each(function(){
+   var newUrl = $(this).prop('href').replace('harvard.catalog', 'harvard-catalog-courses');
+   $(this).prop('href', newUrl);
+ });
+});
