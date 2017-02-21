10f43a1dbf4729be0847b26007b8fb1c57a5d228
Colin Murtaugh <cmurtaugh@gmail.com>
2017-02-21 06:54:02 -0500

    Update global.js

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 $(document).ready(function(e) {
-    var harvardCopy = '<p>Copyright &copy; 2016 The President and Fellows of Harvard College</p>';
+    var harvardCopy = '<p>Copyright &copy; 2017 The President and Fellows of Harvard College</p>';
     harvardCopy += '<p><a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Privacy+Policy+for+Canvas" id="privacy_policy_link">Privacy Policy</a> | <a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Acceptable+Use+Policy+for+Canvas" id="acceptable_use_policy_link">Acceptable Use Policy</a></p>';
     $('footer').html(harvardCopy);
 });
