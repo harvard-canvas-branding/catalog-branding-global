fa27b922c87e585aa23b4b7bb7636a987373cb70
Colin Murtaugh <cmurtaugh@gmail.com>
2019-10-01 10:53:50 -0400

    turn on IP anonymization

--- global.js
+++ global.js
@@ -4,6 +4,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-49649810-11', 'auto');
+ga('set', 'anonymizeIp', true);
 ga('send', 'pageview');
 
 $(document).ready(function(e) {
