e3c14ab7a06d01529819601028aef42d0d16c2c3
Colin Murtaugh <cmurtaugh@gmail.com>
2019-10-07 11:26:39 -0400

    Use the existing GA object and add a custom tracker

--- global.js
+++ global.js
@@ -1,11 +1,9 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-hu_ga('create', 'UA-49649810-11', 'auto');
-hu_ga('set', 'anonymizeIp', true);
-hu_ga('send', 'pageview');
+if (window.ga && ga.loaded) {
+    ga('create', 'UA-49649810-11', 'auto', 'huTracker');
+    ga('huTracker.set', 'anonymizeIp', true);
+    ga('huTracker.send', 'pageview');
+}
 
 $(document).ready(function(e) {
   var copyYear = new Date().getFullYear();
