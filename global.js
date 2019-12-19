c3acf8d3191e53b8126596d40a73eebfba60ab92
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-12-19 10:29:15 -0500

    Small cosmetic tweak

--- global.js
+++ global.js
@@ -8,10 +8,10 @@ if (window.ga && ga.loaded) {
 $(document).ready(function(e) {
   var copyYear = new Date().getFullYear();
 
-  var harvardCopy = '<div>Copyright &copy; 2014-' + copyYear + ' The President and Fellows of Harvard College | ';
-  harvardCopy += '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> | ';
+  var harvardCopy = '<div>Copyright &copy; 2014-' + copyYear + ' The President and Fellows of Harvard College | &nbsp</div>';
+  harvardCopy += '<p><a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> | ';
   harvardCopy += '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="accessibility_link" target="_blank">Accessibility</a> | ';
-  harvardCopy += '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a></div>';
+  harvardCopy += '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a></p>';
   $('footer').html(harvardCopy);
 
   $("div.ic-Login-header__links > a#register_link").remove();
