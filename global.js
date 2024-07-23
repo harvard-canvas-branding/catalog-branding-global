73d88315f0ac87684bd797ea6cf22566862bc7f7
Patrick Tonne <patrick_tonne@harvard.edu>
2024-07-23 15:14:16 -0400

    Prevent loading of custom footer on content migrations page

--- global.js
+++ global.js
@@ -2,11 +2,15 @@
 $(document).ready(function(e) {
   var copyYear = new Date().getFullYear();
 
-  var harvardCopy = '<div>Copyright &copy; 2014-' + copyYear + ' The President and Fellows of Harvard College <span aria-hidden="true">|</span> &nbsp</div>';
-  harvardCopy += '<p><a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> <span aria-hidden="true">|</span> ';
-  harvardCopy += '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="accessibility_link" target="_blank">Accessibility</a> <span aria-hidden="true">|</span> ';
-  harvardCopy += '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a></p>';
-  $('footer').html(harvardCopy);
+  var currentPage = window.location.pathname;
+  var isContentMigrationPage = currentPage.match(/\/courses\/\d+\/content_migrations.*/);
+  if (!isContentMigrationPage) {
+    var harvardCopy = '<div>Copyright &copy; 2014-' + copyYear + ' The President and Fellows of Harvard College <span aria-hidden="true">|</span> &nbsp</div>';
+    harvardCopy += '<p><a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> <span aria-hidden="true">|</span> ';
+    harvardCopy += '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="accessibility_link" target="_blank">Accessibility</a> <span aria-hidden="true">|</span> ';
+    harvardCopy += '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a></p>';
+    $('footer').html(harvardCopy);
+  }
 
   $("div.ic-Login-header__links > a#register_link").remove();
 
